38674193b4e65d298b91bfdbc46383008eed3c60
Sia X <sia.xiong@outlook.com>
2018-10-12 22:06:07 -0700

    Bookmark screen styles

--- app/src/components/Bookmarks.js
+++ app/src/components/Bookmarks.js
@@ -5,6 +5,7 @@ import {
     StyleSheet, 
     Text 
 } from 'react-native'
+import {scale, verticalScale, moderateScale} from '../style-scaling'
 
 class Bookmarks extends Component {
     constructor(props) {
@@ -13,11 +14,14 @@ class Bookmarks extends Component {
     }
     render() { 
         return ( 
-            <View>
+            <View style={styles.container}>
+                {/* Bookmarks List */}
+                <View><Text>dfdgfdg</Text></View>
+
                 {/* Button that navigates to Search screen */}
-                <TouchableOpacity onPress={() => this.props.navigation.navigate('Search')}>
-                    <Text> Add New Place </Text>
-                </TouchableOpacity>
+                    <TouchableOpacity style={styles.button} onPress={() => this.props.navigation.navigate('Search')}>
+                        <Text style={{...styles.font, color: '#FFF'}}> Add New Place </Text>
+                    </TouchableOpacity>
             </View>
          )
     }
@@ -27,5 +31,22 @@ export default Bookmarks
 
 
 const styles = StyleSheet.create({
-
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    button: {
+        width: moderateScale(300),
+        height: verticalScale(45),
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#0404CE',
+        borderRadius: 5,
+        marginBottom: scale(5)
+    },
+    font: {
+        fontFamily: 'HelveticaNeue-Light'
+    }
 })
